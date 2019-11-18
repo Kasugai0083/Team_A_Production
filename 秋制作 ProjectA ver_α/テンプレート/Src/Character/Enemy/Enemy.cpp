730d36cbe7988e0d52a73f177d411bd23e2ad6a2
renonakamoto <obrsk23@yahoo.co.jp>
2019-11-18 17:46:53 +0900

    SpawnTimeとMoveTimer追加

--- 秋制作 ProjectA ver_α/テンプレート/Src/Character/Enemy/Enemy.cpp
+++ 秋制作 ProjectA ver_α/テンプレート/Src/Character/Enemy/Enemy.cpp
@@ -1,10 +1,46 @@
 #include "Enemy.h"
+#include <random>
+#include <time.h>
 
 Enemy::EnemyTimer::~EnemyTimer()
 {
 }
 
-void Enemy::EnemyTimer::TimeCount()
+unsigned int Enemy::EnemyTimer::GetRand(unsigned int min_val_, unsigned int max_val_)
 {
-	
+	static std::mt19937_64 mt64((unsigned int)time(NULL));
+
+	std::uniform_int_distribution<unsigned int> get_rand_uni_int(min_val_, max_val_);
+
+	return get_rand_uni_int(mt64);
+}
+
+bool Enemy::EnemyTimer::SpawnTimer(Enemy& enemy_)
+{
+	// 早期リターン
+	if (enemy_.m_IsDeath == false) { return false; }
+
+	m_Time++;
+
+	if (m_Time >= m_RandTime) {
+		m_Time = 0;
+		return true;
+	}
+
+	return false;
+}
+
+bool Enemy::EnemyTimer::MoveTimer(Enemy& enemy_)
+{
+	// 早期リターン
+	if (enemy_.m_IsDeath == true) { return; }
+
+	m_Time++;
+
+	if (m_Time >= m_RandTime) {
+		m_Time = 0;
+		return true;
+	}
+
+	return false;
 }
--- 秋制作 ProjectA ver_α/テンプレート/Src/Character/Enemy/Enemy.h
+++ 秋制作 ProjectA ver_α/テンプレート/Src/Character/Enemy/Enemy.h
@@ -4,6 +4,7 @@
 #include "../CharacterID.h"
 #include "../../Utility/RoomID.h"
 #include "..//..//Scene/Scene.h"
+#include <random>
 
 
 class Enemy : public Character {
@@ -14,22 +15,40 @@ public:
 
 	virtual void LoadTex(SceneId id_)override {}
 
-private:
+protected:
 	// エネミーの移動用タイマークラス
 	class EnemyTimer
 	{
 	public:
-		EnemyTimer(int time_) {
+		/*
+			コンストラクタ
+		*/
+		EnemyTimer(int time_, int min_time_, int max_time) {
 			m_Time = time_;
+			m_RandTime = GetRand(min_time_, max_time);
 		}
-
+		/*
+			デストラクタ
+		*/
 		~EnemyTimer();
 
-		void TimeCount();
-		
+		unsigned int GetRand(unsigned int min_val_, unsigned int max_val_);
 
-	private:
+		/*
+			スポーン用のタイマー関数
+			特定の時間になるとTrueが返る
+		*/
+		bool SpawnTimer(Enemy& enemy_);
+
+		/*
+			移動用のタイマー関数
+			特定の時間になるとTrueが返る
+		*/
+		bool MoveTimer(Enemy& enemy_);
+
+	protected:
 		int m_Time;
+		int m_RandTime;
 	};
 
 private:
