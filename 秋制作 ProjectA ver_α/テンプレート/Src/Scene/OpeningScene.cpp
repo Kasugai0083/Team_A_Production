08b507f00a06148c6a924597a38837ed8718a452
春日井理元 <kasugai0083@gmail.com>
2019-11-28 14:50:44 +0900

    [#10]値に合わせて描画を変更できるような処理を追加

--- 秋制作 ProjectA ver_α/テンプレート/Src/Scene/OpeningScene.cpp
+++ 秋制作 ProjectA ver_α/テンプレート/Src/Scene/OpeningScene.cpp
@@ -33,10 +33,31 @@ SceneId UpdateOpeningScene()
 	return SceneId::OpeningScene;
 }
 
+enum class Days {
+	DAY_0,
+	DAY_1,
+	DAY_2,
+	DAY_3,
+};
+
+Days days = Days::DAY_0;
+
 void DrawOpeningScene()
 {
-
-	DrawTexture(0.0f, 0.0f, GetTexture(TEXTURE_CATEGORY_OPENING, OpeningCategoryTextureList::OpeningDiaryTex));
+	switch (days)
+	{
+	case Days::DAY_0:
+		DrawTexture(0.0f, 0.0f, GetTexture(TEXTURE_CATEGORY_OPENING, OpeningCategoryTextureList::OpeningDiaryTex));
+		break;
+	case Days::DAY_1:
+		break;
+	case Days::DAY_2:
+		break;
+	case Days::DAY_3:
+		break;
+	default:
+		break;
+	}
 
 }
 
